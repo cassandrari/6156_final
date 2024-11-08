9cb2eff26c82cd057c386f0168b4152d466a76cb
cassandrari <criggs1@uncc.edu>
2024-11-07 19:55:10 -0500

    Update MD.py

--- MD.py
+++ MD.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('Machine_Downtime.csv')
 
 df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
 
