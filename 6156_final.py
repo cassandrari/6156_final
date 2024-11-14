50b6cc8a8d6a24879e7232c78d7ea75e83890521
cassandrari <criggs1@uncc.edu>
2024-11-14 18:28:10 -0500

    Update 6156_final.py

--- 6156_final.py
+++ 6156_final.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+
+#dataset
+machine_data = pd.read_csv('Machine_Downtime.csv')
+machine_data['Date'] = pd.to_datetime(machine_data['Date'])
+
+
+#trend analysis over time 
+downtime_trends = machine_data.groupby('Date')['Downtime'].value_counts().unstack(fill_value=0)
+downtime_trends.columns = ['No Downtime', 'Downtime']  # Rename columns for clarity
+
+fig = px.line(downtime_trends, 
+              x=downtime_trends.index, 
+              y='Downtime', 
+              title=f'Downtime Trend for {machine}',
+              labels={'Date': 'Date', 'Downtime': 'Number of Downtime Events'})
+
+st.plotly_chart(fig)
